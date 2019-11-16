1933f4eb17238374fa112f9a5ac141f0ce89ee2c
rchaser53 <tayoshizawa29@gmail.com>
2019-11-16 16:04:42 +0900

    use reqwest Client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use reqwest;
+use reqwest::{Client, ClientBuilder};
 // use futures::Future;
 // use reqwest::{Response, Error};
 
@@ -7,10 +8,10 @@ fn main() {
     let _ = futures::executor::block_on(task);
 }
 
-async fn foo() -> Result<(), Box<dyn std::error::Error>>{
-  let body = reqwest::get("https://www.rust-lang.org")?
-    .text()?;
+async fn foo() -> Result<(), Box<dyn std::error::Error>> {
+    let builder = Client::builder().build()?;
+    let body = builder.get("https://www.rust-lang.org").send()?.text()?;
 
-  println!("body = {:?}", body);
-  Ok(())
+    println!("body = {:?}", body);
+    Ok(())
 }
