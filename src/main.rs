61419c63bcab4a5b40bc8c2f838048b0e2b25c7a
rchaser53 <tayoshizawa29@gmail.com>
2019-11-16 16:14:33 +0900

    call rakuten api using environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use reqwest;
-use reqwest::{Client, ClientBuilder};
+use reqwest::Client;
+use std::env;
 // use futures::Future;
 // use reqwest::{Response, Error};
 
@@ -9,8 +10,12 @@ fn main() {
 }
 
 async fn foo() -> Result<(), Box<dyn std::error::Error>> {
+    let application_key = (env::var("APPLICATION_KEY"))
+        .expect("should export environment variable APPLICATION_KEY to use RAKUTEN api");
+
+    let endpoint_url = format!("https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&applicationId={}", application_key);
     let builder = Client::builder().build()?;
-    let body = builder.get("https://www.rust-lang.org").send()?.text()?;
+    let body = builder.get(&endpoint_url).send()?.text()?;
 
     println!("body = {:?}", body);
     Ok(())
